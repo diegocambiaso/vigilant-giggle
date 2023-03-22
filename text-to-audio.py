9deb07f48d6a45c82a7ef9bde8500b48c383c14c
Diego Cambiaso <diegocambiaso@gmail.com>
2023-03-22 11:52:28 -0300

    Create text-to-audio.py
    This script is for educational purposes and will let you convert text to audio.

---
+++ text-to-audio.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf8
+#
+# Copyright (C) 2023, Diego Cambiaso
+# GNU General Public License v3.0
+
+'''
+This script is for educational purposes and will let you convert text to a audio.
+'''
+
+from gtts import gTTS
+from playsound import playsound
+
+file = 'interview.mp3'
+speach = gTTS(text='This is the text that you want \
+    to convert into audio file.',
+    lang = 'en', slow = False)
+
+speach.save(audio)
+playsound(audio)
